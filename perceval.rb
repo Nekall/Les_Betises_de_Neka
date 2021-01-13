4993997f946be448b63af4d8713d0ef954703711
Neka <nekoanchor@yahoo.fr>
2021-01-13 21:42:29 +0100

    Perceval by Karadoc

---
+++ perceval.rb
@@ -0,0 +1,11 @@
+loop do
+    random = rand(15)+1
+    if(random == 1)
+    then puts "Non je... connais pas ce mot là."
+          sleep(2)
+    else puts "Ouais, c'est pas faux."
+          sleep(2)
+        end
+end
+
+#La botte secrète de Karadoc
